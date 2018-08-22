70cb7fb9ce98a30e22d2f910bbec8ef63021f40f
lorenzo <l.ansaloni@elsevier.com>
2018-08-22 16:40:37 +0100

    code simplified

--- src/main/scala/com/lansalo/Target.scala
+++ src/main/scala/com/lansalo/Target.scala
@@ -6,13 +6,13 @@ object Target {
 
   // candidate 1
   def mapOnFunctionsAndList(input: String, slice: Int, funcs: List[String => Int]): List[Int] = {
-    funcs.map(f => slide(input, slice).map(f(_)).min)
+    funcs.map(f => slide(input, slice).map(f).min)
   }
 
   // candidate 2
   def foldOnFunctionsList(input: String, slice: Int, funcs: List[String => Int]): List[Int] = {
     funcs.foldLeft(List.empty[Int])((acc, func) => {
-      slide(input, slice).map(str => func(str)).min :: acc
+      slide(input, slice).map(func).min :: acc
     })
   }
 }
